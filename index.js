6715b6503533666876c4746acf989b3ea04e90f4
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-08-26 15:00:28 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+
+function writeCards( arr=["Guadalupe", "Ollie", "Aki"],birthday='birthday'){
+    for(let i=0;i<arr.length;i++){
+        console.log(`Thankyou,${arr[i]} for the wonderful${birthday} gift!`)
+    }
+}
+writeCards()
+const countDown = () => {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
